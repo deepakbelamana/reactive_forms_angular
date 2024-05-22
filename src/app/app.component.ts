13f9cdae40da3fc325025c70eb2e56fda7a5c24f
deepak <saideepak627@gmail.com>
2024-05-22 09:38:59 +0530

    custom validators added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,12 @@ export class AppComponent implements OnInit {
 
   genders = ['male', 'female'];
   rcForm: FormGroup;
-
+  forbiddenName = ['ann','james']
   ngOnInit(): void {
     this.rcForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
-        'email': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required,this.checkUserNameForbidded.bind(this)]),
+        'email': new FormControl(null, [Validators.required]),
       }),
       'hobbies': new FormArray([]),
       'gender': new FormControl('male')
@@ -26,7 +26,7 @@ export class AppComponent implements OnInit {
     console.log(this.rcForm)
   }
 
-  //dynamically adding content in the form 
+  //dynamically adding content in the form
   onAddHobby() {
     const newHobbie = new FormControl(null, Validators.required);
     (<FormArray>this.rcForm.get('hobbies')).push(newHobbie);
@@ -35,5 +35,12 @@ export class AppComponent implements OnInit {
 
   get Controls() {
     return (<FormArray>this.rcForm.get('hobbies')).controls;
+  }
+
+  checkUserNameForbidded(control:FormControl) : {[s:string]:boolean} {
+    if(this.forbiddenName.indexOf(control.value) !== -1) {
+      return {'usernameForbidden' : true}
+    }
+    return null; // either return null or omit return but do not return {usernameForbidden : false}
   }
 }
