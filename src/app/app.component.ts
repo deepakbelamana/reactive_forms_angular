62dc29536659cedbfd68d95f8e162c4313f19878
deepak <saideepak627@gmail.com>
2024-05-24 09:43:35 +0530

    added async validator for email

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,12 @@ export class AppComponent implements OnInit {
 
   genders = ['male', 'female'];
   rcForm: FormGroup;
-  forbiddenName = ['ann','james']
+  forbiddenName = ['ann', 'james']
   ngOnInit(): void {
     this.rcForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, [Validators.required,this.checkUserNameForbidded.bind(this)]),
-        'email': new FormControl(null, [Validators.required]),
+        'username': new FormControl(null, [Validators.required, this.checkUserNameForbidded.bind(this)]),
+        'email': new FormControl(null, [Validators.required, Validators.email],this.checkForbiddenEmail.bind(this)),
       }),
       'hobbies': new FormArray([]),
       'gender': new FormControl('male')
@@ -37,12 +37,25 @@ export class AppComponent implements OnInit {
     return (<FormArray>this.rcForm.get('hobbies')).controls;
   }
 
-  checkUserNameForbidded(control:FormControl) : {[s:string]:boolean} {
-    if(this.forbiddenName.indexOf(control.value) !== -1) {
+  checkUserNameForbidded(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenName.indexOf(control.value) !== -1) {
       // this object will present in the error field of that control ,
       //in the form object when we submit
-      return {'usernameForbidden' : true}
+      return { 'usernameForbidden': true }
     }
     return null; // either return null or omit return but do not return {usernameForbidden : false}
+  }
+
+  checkForbiddenEmail(control: FormControl): Promise<any> {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({ 'emailForbidden': true });
+        } else {
+          resolve(null);
+        }
+      }, 1500)
+    })
+    return promise;
   }
 }
